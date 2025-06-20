809803391f0e9dd1dbb8051d6a0ddfcff7d6fcbf
SonHai4101 <91021500+SonHai4101@users.noreply.github.com>
2025-06-20 17:38:26 +0700

    form create form

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,16 +1,17 @@
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
-import Link from "next/link";
 import React from "react";
 import AppSidebar from "../_components/AppSidebar";
 
 export default function layout({ children }: { children: React.ReactNode }) {
   return (
     <SidebarProvider>
-        <AppSidebar />
-        <main>
+      <AppSidebar />
+      <main className="w-full max-w-[1000px] mx-auto">
+        <div className="max-w-md mx-auto pt-4 px-4 py-17">
           <SidebarTrigger />
           {children}
-        </main>
+        </div>
+      </main>
     </SidebarProvider>
   );
 }
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,183 @@
-import React from 'react'
+"use client";
+import { useState } from "react";
+import { toast } from "sonner";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as z from "zod";
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { Checkbox } from "@/components/ui/checkbox";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 
-export default function page() {
+const formSchema = z.object({
+  title: z.string().min(1),
+  description: z.string().min(1),
+  content: z.string().min(1),
+  isActive: z.unknown(),
+  thumbnail: z.string(),
+  category: z.string(),
+});
+
+export default function MyForm() {
+  const [preview, setPreview] = useState<string | null>(null);
+  const [files, setFiles] = useState<File[] | null>(null);
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    try {
+      console.log(values);
+      toast(
+        <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
+          <code className="text-white">{JSON.stringify(values, null, 2)}</code>
+        </pre>
+      );
+    } catch (error) {
+      console.error("Form submission error", error);
+      toast.error("Failed to submit the form. Please try again.");
+    }
+  }
+
   return (
-    <div>This is dashboard page</div>
-  )
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="space-y-8 w-full mx-auto py-10"
+      >
+        <FormField
+          control={form.control}
+          name="title"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Title</FormLabel>
+              <FormControl>
+                <Input placeholder="Your title" type="" {...field} />
+              </FormControl>
+              {/* <FormDescription>This is your public display name.</FormDescription> */}
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="description"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Description</FormLabel>
+              <FormControl>
+                <Textarea
+                  placeholder="Some description..."
+                  className="resize-none"
+                  {...field}
+                />
+              </FormControl>
+              {/* <FormDescription>You can @mention other users and organizations.</FormDescription> */}
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="content"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Content</FormLabel>
+              <FormControl>
+                <Textarea placeholder="A interesting content here!" className="resize-none" {...field} />
+              </FormControl>
+              {/* <FormDescription>This is your public display name.</FormDescription> */}
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="thumbnail"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Thumbnail</FormLabel>
+              <FormControl>
+                <Input placeholder=""
+                type="file"
+                // onChange={e => {field.onChange(e.target.files);}}
+                multiple
+                {...field} />
+              </FormControl>
+              <FormDescription>
+                This is your public display thumbnail.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="isActive"
+          render={({ field }) => (
+            <FormItem className="flex flex-row items-start space-x-3 space-y-0 rounded-md border p-4">
+              <FormControl>
+                <Checkbox
+                  // checked={checked}
+                  onCheckedChange={field.onChange}
+                />
+              </FormControl>
+              <div className="space-y-1 leading-none">
+                <FormLabel>Is Active</FormLabel>
+                <FormDescription>
+                  Checked if the post is active or not.
+                </FormDescription>
+                <FormMessage />
+              </div>
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="category"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Category</FormLabel>
+              <Select onValueChange={field.onChange} defaultValue={field.value}>
+                <FormControl>
+                  <SelectTrigger>
+                    <SelectValue placeholder="Select a category" />
+                  </SelectTrigger>
+                </FormControl>
+                <SelectContent>
+                  <SelectItem value="category1">category 1</SelectItem>
+                  <SelectItem value="category2">category 2</SelectItem>
+                  <SelectItem value="category3">category 3</SelectItem>
+                </SelectContent>
+              </Select>
+              {/* <FormDescription>You can manage email addresses in your email settings.</FormDescription> */}
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  );
 }
